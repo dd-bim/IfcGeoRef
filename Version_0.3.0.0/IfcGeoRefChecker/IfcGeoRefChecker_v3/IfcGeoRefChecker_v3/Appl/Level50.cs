91f529f185ed1af5664559c035d01541373dc3ff
goerne <hendrik.goerne@htw-dresden.de>
2019-05-02 09:26:56 +0200

    Bugfix Equalizer (for Comparing tool)

--- Version_0.3.0.0/IfcGeoRefChecker/IfcGeoRefChecker_v3/IfcGeoRefChecker_v3/Appl/Level50.cs
+++ Version_0.3.0.0/IfcGeoRefChecker/IfcGeoRefChecker_v3/IfcGeoRefChecker_v3/Appl/Level50.cs
@@ -1,12 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Windows;
-using Xbim.Ifc;
-using Xbim.Ifc4.Interfaces;
-using Xbim.Ifc4.MeasureResource;
-using Xbim.Ifc4.PropertyResource;
-using Xbim.Ifc4.RepresentationResource;
 
 namespace IfcGeoRefChecker.Appl
 {
@@ -47,8 +40,6 @@ namespace IfcGeoRefChecker.Appl
             if(Translation_Eastings == other.Translation_Eastings &&
                 Translation_Northings == other.Translation_Northings &&
                 Translation_Orth_Height == other.Translation_Orth_Height &&
-                RotationXY[0] == other.RotationXY[0] &&
-                RotationXY[1] == other.RotationXY[1] &&
                 Scale == other.Scale &&
                 string.Equals(CRS_Name, other.CRS_Name) &&
                 string.Equals(CRS_Description, other.CRS_Description) &&
@@ -57,7 +48,27 @@ namespace IfcGeoRefChecker.Appl
                 string.Equals(CRS_Projection_Name, other.CRS_Projection_Name) &&
                 string.Equals(CRS_Projection_Zone, other.CRS_Projection_Zone))
             {
-                return true;
+                if((RotationXY == null && other.RotationXY == null) || (RotationXY[0] == other.RotationXY[0] && RotationXY[1] == other.RotationXY[1]))
+                {
+                    return true;
+                }
+                else
+                    return false;
+
+                //if(RotationXY == null && other.RotationXY != null)
+                //{
+                //    return false;
+                //}
+                //else if(RotationXY != null && other.RotationXY == null)
+                //{
+                //    return false;
+                //}
+                //else if(RotationXY[0] == other.RotationXY[0] && RotationXY[1] == other.RotationXY[1])
+                //{
+                //    return true;
+                //}
+                //else
+                //    return false;
             }
             else
             {
